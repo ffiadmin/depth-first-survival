a0dfe8b61c7d9089629fde529bce9f791056c18b
Sariander <garrett.young17@gmail.com>
2014-03-23 21:31:22 -0400

    Third Person Camera Class, just in case

---
+++ TPCamera.cpp
@@ -0,0 +1,42 @@
+#include "TPCamera.h"
+
+TPCamera::TPCamera() { }
+
+TPCamera::~TPCamera() { }
+
+void TPCamera::resize(D3DXMATRIX &Proj, float FOV, float nearClip, float farClip) {
+	float aspectRatio = (float)windowWidth/windowHeight;
+	D3DXMatrixPerspectiveFovLH(&Proj, FOV, aspectRatio, nearClip, farClip);
+}
+
+void TPCamera::update(float &theta, float &phi, float &radius, float headPos, float &dt, GameObject player, D3DXMATRIX &view, D3DXVECTOR3 &eyePos)
+{
+	if(GetAsyncKeyState('L') & 0x8000)	theta -= 2.0f*dt;
+	if(GetAsyncKeyState('J') & 0x8000)	theta += 2.0f*dt;
+	if(GetAsyncKeyState('I') & 0x8000)	phi -= 2.0f*dt;
+	if(GetAsyncKeyState('K') & 0x8000)	phi += 2.0f*dt;
+	if(GetAsyncKeyState('O') & 0x8000)	radius -= 25.0f*dt;
+	if(GetAsyncKeyState('P') & 0x8000)	radius += 25.0f*dt;
+
+	// Restrict the angle mPhi and radius mRadius.
+	if( phi < 0.1f )	phi = 0.1f;
+	if( phi > PI-0.1f)	phi = PI-0.1f;
+
+	//Loop mTheta back to zero for maths sake
+	if( theta >= (PI * 2) ) theta = ToRadian(0.1f);
+	if( theta <= 0.0f ) theta = ToRadian(359.99f);
+
+	if( radius < 0.0f) radius = 0.0f;
+
+	eyePos.x =  radius*sinf(phi)*sinf(theta) + player.getPosition().x;
+	eyePos.z = -radius*sinf(phi)*cosf(theta) + player.getPosition().z;;
+	eyePos.y =  radius*cosf(phi) + player.getPosition().y;
+
+	// Build the view matrix.
+	D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+	target.x = player.getPosition().x;
+	target.y = player.getPosition().y;
+	target.z = player.getPosition().z;
+	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+	D3DXMatrixLookAtLH(&view, &eyePos, &target, &up);
+}
---
+++ TPCamera.h
@@ -0,0 +1,30 @@
+#pragma once
+#define _USE_MATH_DEFINES
+
+#include <cmath>
+#include <d3dx9math.h>
+#include "d3dApp.h"
+#include "d3dUtil.h"
+#include "constants.h"
+
+//Include the class for the player here
+//Change class of player variable accordingly
+#include "GameObject.h"
+
+class TPCamera {
+public : 
+	TPCamera();
+	~TPCamera();
+
+	//Call this in onResize
+	void resize(D3DXMATRIX &mProj, float FOV, float nearClip, float farClip);
+	//Call this in initApp
+	void setHeightAndWidth(int wH, int wW) { windowHeight = wH; windowWidth = wW;}
+	//Call this in update
+	void update(float &theta, float &phi, float &radius, float headPos, float &dt, GameObject player, D3DXMATRIX &view, D3DXVECTOR3 &eyePos);
+
+private : 
+	int windowHeight;
+	int windowWidth;
+};
+
